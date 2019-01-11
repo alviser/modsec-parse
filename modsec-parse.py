28c2aa8992f3a0cb4d795044cf27ba5e1550f484
alviser <alvise@rabitti.org>
2019-01-11 15:25:48 +0100

    added URL and method filtering

--- modsec-parse.py
+++ modsec-parse.py
@@ -15,25 +15,63 @@ def get_options(cmd_args=None):
         help="""a text file where each line holds a different URL where to search for WP""",
         type=str,
         default='modsec_audit.log')
+    cmd_parser.add_argument(
+        '-g',
+        '--grep',
+        help="""show all entries with URL matching the given string""",
+        type=str,
+        default='')
+    cmd_parser.add_argument(
+        '-m',
+        '--method',
+        help="""show all entries with requests using the given method""",
+        type=str,
+        default='')
 
     args = cmd_parser.parse_args(cmd_args)
 
     options = {}
     options['input_log_file'] = args.input_log_file
+    options['grep'] = args.grep
+    options['method'] = args.method
 
     return options
+
+def filterByMatchingURL(logs,s):
+    entries = {}
+    for e in logs:
+        if (s in logs[e]['request']['url']):
+            entries[e]  = logs[e]
+    return entries
+
+def filterByMatchingMethod(logs,m):
+    entries = {}
+    for e in logs:
+        if (m.upper() in logs[e]['request']['method'].upper()):
+            entries[e]  = logs[e]
+    return entries
 
 def main(opts):
 
     f = open(opts['input_log_file'],"r")
-
     log = ModsecParser.parseFile(f)
-
     f.close()
     
+    if (opts['grep'] != ""):
+        log = filterByMatchingURL(log,opts['grep'])
+
+    if (opts['method'] != ""):
+        log = filterByMatchingMethod(log,opts['method'])
+
+    # output
     for e in log:
-        if ('client_ip' in log[e]['modsec_info']):
-            print("entry id: " + log[e]['general_info']['uniqid'] + "\tclient ip: " + log[e]['general_info']['client_ip'] + "\tmodsec ip: " + log[e]['modsec_info']['client_ip'])
+        print(log[e]['request']['method'] + "\t" + log[e]['request']['url'])
+        if (('rule_id' in log[e]['modsec_info']) and ('msg' in log[e]['modsec_info'])):
+            print(" " + log[e]['modsec_info']['rule_id'] + "\t" + log[e]['modsec_info']['msg'])
+        elif ('Apache-Error' in log[e]['modsec_info']):
+            print(" " + log[e]['modsec_info']['Apache-Error'])
+        else:
+            print(" " + str(log[e]['modsec_info']))
 
 if __name__ == "__main__":
     sys.exit(main(get_options()))
